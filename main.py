2120a15f7bc378494ca0dddafeffc715a3b2a2ca
Alena <111291767+MyatnoeMorozhenko@users.noreply.github.com>
2023-09-27 13:19:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ photo = open('DairyTech.jpeg', 'rb')
 
 @dp.message_handler(commands=['start'])
 async def buy(msg: types.Message):
-    user_id = msg.from_user
+    user_id = msg.from_user.id
     with open('DairyTech.jpeg', 'rb') as photo:
         await bot.send_photo(msg.from_user.id, caption = f"Добрый день, {msg.from_user.full_name}\!\n\nРады приветсвовать в нашем онлайн бизнес сообществе **DairyTech Connect** для специалистов молочной отрасли\.\n\n • Отраслевое сообщество для делового общения\n • Площадка для обмена новостями, экспертизой, аналитикой индустрии\n • Профильные онлайн\-мероприятия\n\n||Перейти на страницу сообщества можно, нажав на кнопку DairyTech Connect или по [ссылке](https://app.dairytech-connect.com/event/1099/feed)||",
                          parse_mode='MarkdownV2',photo=photo,
