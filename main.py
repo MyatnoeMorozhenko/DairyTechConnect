82ad4ccf4c381b8536368dea5c19a6dd9db1fc17
Alena <111291767+MyatnoeMorozhenko@users.noreply.github.com>
2023-09-27 13:51:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ async def buy(msg: types.Message):
                          parse_mode='MarkdownV2',photo=photo,
                          reply_markup=keyb_client)
     
-    db_object.execute(f"SELECT id FROM users01 WHERE id = {user_id}")
+    db_object.execute(f"SELECT id FROM user WHERE id = {user_id}")
     result = db_object.fetchone()
     
     if not result:
-        db_object.execute("INSERT INTO users01 (id, username) VALUES (%s, %s)", (user_id, username))
+        db_object.execute("INSERT INTO user (id, username) VALUES (%s, %s)", (user_id, username))
         db.commit()
 
 #WebApp handler
